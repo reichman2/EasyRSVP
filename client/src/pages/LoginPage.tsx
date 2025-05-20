6757664504cea1f4d5eca50594f2ada502672994
Brian Reich <reichman213@gmail.com>
2025-05-20 14:51:51 -0600

    Create Pages

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+        </div>
+    );
+}
+
+
+export default LoginPage;
---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,17 @@
+
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h1>Register</h1>
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button>Register</button>
+        </div>
+    );
+}
+
+export default RegisterPage;
