21aefe8c981239fdbad75c60443255aa14e80a5e
Brian Reich <reichman213@gmail.com>
2025-05-20 15:10:25 -0600

    Add card to login and register pages

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,12 +1,15 @@
+import Card from "../components/Card";
 
 
 const LoginPage = () => {
     return (
         <div>
-            <h1>Login</h1>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Login</button>
+            <Card className="w-md m-auto mt-50">
+                <h1>Login</h1>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Login</button>
+            </Card>
         </div>
     );
 }
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,15 +1,18 @@
+import Card from "../components/Card";
 
 
 const RegisterPage = () => {
     return (
         <div>
-            <h1>Register</h1>
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
-            <button>Register</button>
+            <Card className="w-md m-auto mt-50">
+                <h1>Register</h1>
+                <input type="text" placeholder="First Name" />
+                <input type="text" placeholder="Last Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <input type="password" placeholder="Confirm Password" />
+                <button>Register</button>
+            </Card>
         </div>
     );
 }
