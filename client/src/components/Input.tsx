902306e3790459c3eecd72e428601615812bbbbc
Brian Reich <reichman213@gmail.com>
2025-05-23 17:52:22 -0600

    additional props values

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -4,15 +4,19 @@ import React from 'react';
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     className?: string;
     label?: string;
-    error?: string;
+    error?: boolean;
 }
 
-const Input = ({ className, placeholder, type }: InputProps) => {
+const Input = ({ className, label, placeholder, type, autoFocus, value, onChange, error }: InputProps) => {
     return (
         <input 
-            className={ `${className} border border-gray-300 rounded-sm p-1 text-sm w-full outline-0 focus:border-teal-700 focus:border-2` }
+            className={ `${className} block w-full border-0 rounded-md bg-white p-1.5 text-sm outline-1 -outline-offset-1 ${error? "outline-red-600" : "outline-gray-300"} focus:outline-teal-700 focus:outline-2 focus:-outline-offset-2` }
             placeholder={ placeholder }
             type={ type }
+            aria-label={ label }
+            autoFocus={ autoFocus }
+            value={ value }
+            onChange={ onChange }
         />
     );
 }
