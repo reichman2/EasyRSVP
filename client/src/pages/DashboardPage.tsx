a182cc8fdbf6308d6da0231e7657090d023ab90b
Brian Reich <reichman213@gmail.com>
2025-07-04 16:00:59 -0600

    Add RSVPs to dashboard

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
-import { getEvents } from "../api/apiService";
+import { getEvents, getRsvps } from "../api/apiService";
 import { useEffect, useState } from "react";
-import { Event } from "../utils/types";
-import { LuEllipsis } from "react-icons/lu";
+import { Event, RSVP } from "../utils/types";
+import { LuEllipsis, LuPencil } from "react-icons/lu";
+import Badge from "../components/Badge";
 
 const DashboardPage = () => {
     const [events, setEvents] = useState<any>({});
+    const [rsvps, setRsvps] = useState<any>({});
 
     const fetchEvents = async () => {
         let eventData;
@@ -24,10 +26,29 @@ const DashboardPage = () => {
         return eventData;
     };
 
+    const fetchRsvps = async () => {
+        let rsvpData;
+        try {
+            rsvpData = await getRsvps();
+        } catch (err: any) {
+            console.error("Error fetching RSVPs:", err.message);
+            console.error(err);
+        }
+
+        if (rsvpData) {
+            setRsvps(rsvpData);
+        }
+
+        console.log("Fetched RSVPs:", rsvpData);
+        return rsvpData;
+    };
+
     // Fetch events when the component mounts
     useEffect(() => {
         fetchEvents();
+        fetchRsvps();
     }, []);
+
 
     return (
         <div className="">
@@ -41,6 +62,16 @@ const DashboardPage = () => {
                         { events?.events?.length > 0 ? (
                             eventList(events)
                         ) : (<p>No events found.</p>) }
+                    </div>
+                </div>
+
+                <div className="m-6 bg-gray-100 rounded-md p-2">
+                    {/* RSVP Details */}
+                    <h1 className="text-2xl mb-4"><a href="/rsvps">My RSVPs</a></h1>
+                    <div className="">
+                        { rsvps?.rsvps?.length > 0 ? (
+                            rsvpList(rsvps)
+                        ) : (<p>No RSVPs found.</p>) }
                     </div>
                 </div>
             </div>
@@ -72,7 +103,7 @@ const eventList = (events: any) => {
         }
 
         return (
-            <li className="bg-white shadow-md rounded-lg p-4 grow">
+            <li className="bg-white shadow-md rounded-lg p-4 grow" key={ key }>
                 <div className="flex flex-row justify-between">
                     <h2 className="text-lg">{ event.title }</h2>
                     <span><LuEllipsis /></span>
@@ -104,6 +135,49 @@ const eventList = (events: any) => {
     );
 }
 
+const rsvpList = (rsvps: { rsvps: RSVP[], length: number, message?: string }) => {
+    const rsvpListItem = (rsvp: RSVP, key: number) => {
+        const status = rsvp.status;
+        const eventTitle = rsvp.event?.title || "Unknown Event";
+        const eventDate = rsvp.event?.startDate? new Date(rsvp.event.startDate) : "Unknown Date";
+        const formattedDate = eventDate instanceof Date ? formatDate(eventDate) : eventDate;
+
+        const STATUS_BADGES = {
+            ACCEPTED: <Badge text="Accepted" color="green" />,
+            MAYBE: <Badge text="Maybe" color="yellow" />,
+            DECLINED: <Badge text="Declined" color="red" />,
+            PENDING: <Badge text="Pending" color="gray" />
+        }
+
+        return (
+            <li className="bg-white shadow-md rounded-lg p-4 grow" key={ key }>
+                <div className="flex flex-row justify-between">
+                    <h2 className="text-lg">{ eventTitle }</h2>
+                    <span className="text-sm"><LuPencil /></span>
+                </div>
+
+                <hr className="bg-gray-300 border-0 h-px mb-3" />
+                <div>
+                    <p className="text-xs text-gray-500">{ formattedDate }</p>
+                    <p className="text-xs text-gray-500">{ rsvp.event?.location }</p>
+                </div>
+
+                <div className="mt-2">
+                    { STATUS_BADGES[status] }
+                </div>
+            </li>
+        )
+    };
+
+    return (
+        <ul className="flex flex-row gap-4 flex-wrap justify-center">
+            { rsvps.rsvps.map((rsvp: RSVP, idx: number) => {
+                return rsvpListItem(rsvp, idx);
+            }) }
+        </ul>
+    )
+}
+
 
 const formatDate = (date: Date) => {
     // TODO should this be a utility function? (moved to utils dir)
@@ -117,6 +191,12 @@ const formatDate = (date: Date) => {
     });
 
     return formatter.format(date);
+}
+
+const titleCase = (str: string) => {
+    return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 }
 
 export default DashboardPage;
