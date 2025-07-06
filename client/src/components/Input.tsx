770ff105282c235d48968672f4d80607be5aaf9b
Brian Reich <reichman213@gmail.com>
2025-07-05 20:37:04 -0600

    Add name field support

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -7,10 +7,11 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     error?: boolean;
 }
 
-const Input = ({ className, label, placeholder, type, autoFocus, value, onChange, error }: InputProps) => {
+const Input = ({ className, label, placeholder, type, autoFocus, value, name, onChange, error }: InputProps) => {
     return (
         <input 
             className={ `${className} block w-full border-0 rounded-md bg-white p-1.5 text-sm outline-1 -outline-offset-1 ${error? "outline-red-600" : "outline-gray-300"} focus:outline-teal-700 focus:outline-2 focus:-outline-offset-2` }
+            name={ name }
             placeholder={ placeholder }
             type={ type }
             aria-label={ label }
